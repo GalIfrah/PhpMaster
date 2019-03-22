ceb435a08f76b69e0c1da0b6b3c1f5421b514c29
gal ifrah <youremail@domain.com>
2019-03-22 14:53:56 +0200

    change submit name in order to add login form too

--- FormTests.php
+++ FormTests.php
@@ -2,83 +2,86 @@
     
 
     <?php
-//
-//    global $_POST;
-//
-//        global $cookie_image;
-//
-//        $cookie_image = "<img src='images/087d17eb-500e-4b26-abd1-4f9ffa96a2c6.jpg'>";
-//
-//
-//    if (isset($_POST['submit'])) {
-//
-//
-//        if (isset($_POST['cookies'])){
-//
-//            setcookie("accepted_cookies", "send_cookie",exp(time() + 60*60*24*7));
-//
-//        }
-//
-//
-//
-//    }
-//
-//
-//
-//    if (isset($_COOKIE['accepted_cookies'])){
-//
-//
-//        echo $cookie_image;
-//
-//    }
 
-        $servername = "localhost";
-        $username = "newuser";
-        $password = "newpass";
-        $dbname = "newdb";
+    require ("DbService.php");
 
-    If(isset($_POST['submit'])){
+    $db_service = new dbService();
+
+    $conn = $db_service->MysqlConnect("localhost", "newuser", "newpass", "newdb");
+
+
+
+
+    // create new user
+
+    If(isset($_POST['submit_registration'])){
+
         try {
 
 
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $stmt = $conn->prepare("INSERT INTO caloriesCalc (id, firstname, lastname, email,password, phone)
+                    VALUES (:id, :firstname, :lastname, :email, :password, :phone)");
 
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            // prepare sql and bind parameters
-            $stmt = $conn->prepare("INSERT INTO caloriesCalc (id, firstname, lastname, email, phone)
-    VALUES (:id, :firstname, :lastname, :email, :phone)");
             $stmt->bindParam(':id', $id);
             $stmt->bindParam(':firstname', $firstname);
             $stmt->bindParam(':lastname', $lastname);
             $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':password', $password);
             $stmt->bindParam(':phone', $phone);
 
             $id = $_POST['id'];
             $firstname = $_POST['firstname'];
             $lastname= $_POST['lastname'];
             $email= $_POST['email'];
+            $password= $_POST['password'];
             $phone= $_POST['phone'];
 
             $stmt->execute();
-            echo "New records created successfully";
+
+            echo "New user created successfully";
+
         }catch(PDOException $e){
+
             echo "Error: " . $e->getMessage();
         }
+
         $conn = null;
     }
 
-//    $servername = "localhost";
-//    $username = "newuser";
-//    $password = "newpass";
-//    $dbname = "newdb";
-//
+
+
+    // check if user exist
+
+
+    if(isset($_POST['submit_login'])){
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+
+        $sql = "SELECT email, password, firstname FROM caloriesCalc WHERE email='$email'";
+
+        $stmt = $conn->query($sql);
+
+        $stmt->execute();
+        // use exec() because no results are returned
+        $results = $stmt->fetch();
+
+        if($results['email'] == $email && $results['password'] == $password){
+
+            echo "Hello ". $results['firstname'];
+        }
+
+        else{
+
+            echo "one of your credentials wrong - please try again";
+        }
+    }
+
+
+
 //
 //    try {
-//        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-//        // set the PDO error mode to exception
-//        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //
 //        // sql to create table
 //        $sql = "SELECT * FROM caloriesCalc WHERE id = 1";
