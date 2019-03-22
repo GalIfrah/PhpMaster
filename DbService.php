050287bbf59eaa779f0571e4ee50a6e4f528bfc2
gal ifrah <youremail@domain.com>
2019-03-22 14:53:03 +0200

    change nothing

--- DbService.php
+++ DbService.php
@@ -1,9 +1,56 @@
  <?php
 
-    echo "gal";
+  class dbService{
 
 
-    function createDB(){
+
+    static function MysqlConnect($host, $username, $password, $dbname){
+
+
+        if($dbname != null){
+
+            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+        }
+
+
+        else{
+
+            $conn = new PDO("mysql:host=$host", $username, $password);
+        }
+
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+        return $conn;
+    }
+
+
+    static function createDB(){
+
+        $host="localhost";
+
+        $root="root";
+        $root_password="";
+
+        $user='newuser';
+        $pass='newpass';
+        $db="newdb";
+
+        try {
+            $dbh = new PDO("mysql:host=$host", $root, $root_password);
+
+            $dbh->exec("CREATE DATABASE `$db`;
+                        CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';
+                        GRANT ALL ON `$db`.* TO '$user'@'localhost';
+                        FLUSH PRIVILEGES;")
+            or die(print_r("gal", false));
+
+        } catch (PDOException $e) {
+            die("DB ERROR: ". $e->getMessage());
+        }
+
 
     }
 
@@ -14,5 +61,7 @@
 
 
      function insert(){
-//zx
      }
+
+}
+
